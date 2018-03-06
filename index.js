9b42ef643ca4d9735a541bb16b4e3c29a39fee66
Eva Leake <teasacuraster@gmail.com>
2018-03-06 15:10:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,3 +29,8 @@ function prependKitten(name) {
   var newKittens = [name, ...kittens]
   return newKittens
 }
+
+function removeLastKitten() {
+  var newKittens = kittens.slice()
+  return newKittens
+}
